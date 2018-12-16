8544578b1575c5737b8764c329a6d05d80f9064d
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2018-12-16 18:32:30 +0100

    sise-read-util: Implement it.

--- sise-read-util/src/lib.rs
+++ sise-read-util/src/lib.rs
@@ -18,11 +18,11 @@ pub enum ReadError {
     ExpectedList {
         pos: Option<sise::Pos>,
     },
+    ExpectedListEnd {
+        node_pos: Option<sise::Pos>,
+    },
     ExpectedNodeInList {
         list_pos: Option<sise::Pos>,
-    },
-    UnexpectedNodeInList {
-        node_pos: Option<sise::Pos>,
     },
     InvalidValue {
         value_type: String,
@@ -51,6 +51,15 @@ impl std::fmt::Display for ReadError {
                     f.write_str("expected list")
                 }
             }
+            ReadError::ExpectedListEnd { node_pos } => {
+                if let Some(node_pos) = node_pos {
+                    write!(f, "unexpected node in list at {}:{}",
+                           sise::ReprPosValue(node_pos.line),
+                           sise::ReprPosValue(node_pos.column))
+                } else {
+                    f.write_str("unexpected node in list")
+                }
+            }
             ReadError::ExpectedNodeInList { list_pos } => {
                 if let Some(list_pos) = list_pos {
                     write!(f, "expected node in list at {}:{}",
@@ -58,15 +67,6 @@ impl std::fmt::Display for ReadError {
                            sise::ReprPosValue(list_pos.column))
                 } else {
                     f.write_str("expected node in list")
-                }
-            }
-            ReadError::UnexpectedNodeInList { node_pos } => {
-                if let Some(node_pos) = node_pos {
-                    write!(f, "unexpected node in list at {}:{}",
-                           sise::ReprPosValue(node_pos.line),
-                           sise::ReprPosValue(node_pos.column))
-                } else {
-                    f.write_str("unexpected node in list")
                 }
             }
             ReadError::InvalidValue { value_type, pos } => {
@@ -88,10 +88,373 @@ impl std::error::Error for ReadError {
         match self {
             ReadError::ExpectedAtom { .. } => "expected atom",
             ReadError::ExpectedList { .. } => "expected list",
+            ReadError::ExpectedListEnd { .. } => "unexpected node in list",
             ReadError::ExpectedNodeInList { .. } => "expected node in list",
-            ReadError::UnexpectedNodeInList { .. } => "unexpected node in list",
             ReadError::InvalidValue { .. } => "invalid value",
         }
     }
 }
 
+/// Utility to read nodes.
+///
+/// See `as_atom` and `as_list` methods.
+#[derive(Clone, Debug)]
+pub struct NodeReadUtil<'a, 'b> {
+    node: &'a sise::Node,
+    pos_tree: Option<&'b sise::PosTree>,
+}
+
+impl<'a, 'b> NodeReadUtil<'a, 'b> {
+    #[inline]
+    pub fn new(node: &'a sise::Node, pos_tree: Option<&'b sise::PosTree>) -> Self {
+        Self {
+            node: node,
+            pos_tree: pos_tree,
+        }
+    }
+
+    /// Returns the node in this utility.
+    #[inline]
+    pub fn node(&self) -> &'a sise::Node {
+        self.node
+    }
+
+    /// Returns the position tree of the node in this utility.
+    #[inline]
+    pub fn pos_tree(&self) -> Option<&'b sise::PosTree> {
+        self.pos_tree
+    }
+
+    /// If the node is an atom, returns a `AtomNodeReadUtil` with such
+    /// atom. Otherwise, it returns a `ReadError::ExpectedAtom` error.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!("example");
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// assert_eq!(node_read_util.as_atom().unwrap().atom(), "example");
+    /// ```
+    pub fn as_atom(&self) -> Result<AtomNodeReadUtil<'a>, ReadError> {
+        match self.node {
+            sise::Node::Atom(ref atom) => {
+                let pos = self.pos_tree.map(|pos_tree| pos_tree.pos);
+                Ok(AtomNodeReadUtil::new(atom.as_str(), pos))
+            }
+            _ => Err(ReadError::ExpectedAtom {
+                pos: self.pos_tree.map(|pos_tree| pos_tree.pos),
+            }),
+        }
+    }
+
+    /// If the node is a list, returns a `ListNodeReadUtil` with such
+    /// list. Otherwise, it returns a `ReadError::ExpectedList` error.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!(["example"]);
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// assert_eq!(node_read_util.as_list().unwrap().list(), [sise_expr!("example")]);
+    /// ```
+    pub fn as_list(&self) -> Result<ListNodeReadUtil<'a, 'b>, ReadError> {
+        match self.node {
+            sise::Node::List(ref list) => {
+                Ok(ListNodeReadUtil::new(list.as_slice(), self.pos_tree))
+            }
+            _ => Err(ReadError::ExpectedList {
+                pos: self.pos_tree.map(|pos_tree| pos_tree.pos),
+            }),
+        }
+    }
+}
+
+/// Utility to read atom nodes.
+#[derive(Clone, Debug)]
+pub struct AtomNodeReadUtil<'a> {
+    atom: &'a str,
+    pos: Option<sise::Pos>,
+}
+
+impl<'a> AtomNodeReadUtil<'a> {
+    #[inline]
+    pub fn new(atom: &'a str, pos: Option<sise::Pos>) -> Self {
+        Self {
+            atom: atom,
+            pos: pos,
+        }
+    }
+
+    /// Returns the atom value in this utility.
+    #[inline]
+    pub fn atom(&self) -> &'a str {
+        self.atom
+    }
+
+    /// Returns the position of the atom in this utility.
+    #[inline]
+    pub fn pos(&self) -> Option<sise::Pos> {
+        self.pos
+    }
+
+    /// Decodes the atom using an user-provided function.
+    ///
+    /// The function shall return `None` if the decodification
+    /// fails. In such case, `ReadError::InvalidValue` will be
+    /// returned, with the value of `value_type`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!("example");
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// let atom_read_util = node_read_util.as_atom().unwrap();
+    /// let decoded = atom_read_util.decode(|atom| Some(atom.len()), "decode_as_atom").unwrap();
+    /// assert_eq!(decoded, 7);
+    /// ```
+    pub fn decode<T, F>(&self, f: F, value_type: &str)
+        -> Result<T, ReadError>
+        where F: FnOnce(&str) -> Option<T>
+    {
+        if let Some(value) = f(self.atom) {
+            Ok(value)
+        } else {
+            Err(ReadError::InvalidValue {
+                value_type: value_type.to_string(),
+                pos: self.pos,
+            })
+        }
+    }
+}
+
+/// Utility to read list nodes.
+#[derive(Clone, Debug)]
+pub struct ListNodeReadUtil<'a, 'b> {
+    list: &'a [sise::Node],
+    pos_tree: Option<&'b sise::PosTree>,
+    index: usize,
+}
+
+impl<'a, 'b> ListNodeReadUtil<'a, 'b> {
+    #[inline]
+    pub fn new(list: &'a [sise::Node], pos_tree: Option<&'b sise::PosTree>) -> Self {
+        Self {
+            list: list,
+            pos_tree: pos_tree,
+            index: 0,
+        }
+    }
+
+    /// Returns the list in this utility.
+    #[inline]
+    pub fn list(&self) -> &'a [sise::Node] {
+        self.list
+    }
+
+    /// Returns the position tree of the list in this utility.
+    #[inline]
+    pub fn pos_tree(&self) -> Option<&'b sise::PosTree> {
+        self.pos_tree
+    }
+
+    /// Checks if all the nodes in the list have been read.
+    /// If not, it returns a `ReadError::ExpectedListEnd` error.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!([]);
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// let list_read_util = node_read_util.as_list().unwrap();
+    /// assert!(list_read_util.expect_end().is_ok());
+    /// ```
+    pub fn expect_end(&self) -> Result<(), ReadError> {
+        if self.index == self.list.len() {
+            Ok(())
+        } else {
+            Err(ReadError::ExpectedListEnd {
+                node_pos: self.pos_tree.map(|pos_tree| pos_tree.children[self.index].pos)
+            })
+        }
+    }
+
+    /// Reads the next item in the list. If none are left, it
+    /// returns `None`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!(["a", "b"]);
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// let mut list_read_util = node_read_util.as_list().unwrap();
+    ///
+    /// assert_eq!(list_read_util.try_next_item().unwrap().node(), "a");
+    /// assert_eq!(list_read_util.try_next_item().unwrap().node(), "b");
+    /// assert!(list_read_util.try_next_item().is_none());
+    /// ```
+    pub fn try_next_item(&mut self) -> Option<NodeReadUtil<'a, 'b>> {
+        if self.index == self.list.len() {
+            None
+        } else {
+            let item_pos_tree = self.pos_tree.map(|pos_tree| &pos_tree.children[self.index]);
+            let node = NodeReadUtil::new(&self.list[self.index], item_pos_tree);
+            self.index += 1;
+            Some(node)
+        }
+    }
+
+    /// Reads the next item in the list. If none are left, it
+    /// returns a `ReadError::ExpectedNodeInList` error.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!(["a", "b"]);
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// let mut list_read_util = node_read_util.as_list().unwrap();
+    ///
+    /// assert_eq!(list_read_util.next_item().unwrap().node(), "a");
+    /// assert_eq!(list_read_util.next_item().unwrap().node(), "b");
+    /// ```
+    pub fn next_item(&mut self) -> Result<NodeReadUtil<'a, 'b>, ReadError> {
+        self.try_next_item().ok_or_else(|| ReadError::ExpectedNodeInList {
+            list_pos: self.pos_tree.map(|pos_tree| pos_tree.pos)
+        })
+    }
+
+    /// Gets the remaining items from the list, checks if they
+    /// are atoms and decodes their value using `f`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use sise::sise_expr;
+    ///
+    /// let node = sise_expr!(["1", "12", "123"]);
+    /// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+    /// let list_read_util = node_read_util.as_list().unwrap();
+    /// let decoded = list_read_util.decode_atoms(|atom| Some(atom.len()), "decode_as_atom", false).unwrap();
+    /// assert_eq!(decoded, [1, 2, 3]);
+    /// ```
+    pub fn decode_atoms<T, F>(self, mut f: F, value_type: &str, can_be_empty: bool)
+        -> Result<Vec<T>, ReadError>
+        where F: FnMut(&str) -> Option<T>
+    {
+        let pos_tree = self.pos_tree;
+        let mut values = Vec::with_capacity(self.list.len() - self.index);
+        for item in self {
+            values.push(item.as_atom()?.decode(|atom| f(atom), value_type)?);
+        }
+        if !can_be_empty && values.is_empty() {
+            Err(ReadError::ExpectedNodeInList {
+                list_pos: pos_tree.map(|pos_tree| pos_tree.pos),
+            })
+        } else {
+            Ok(values)
+        }
+    }
+}
+
+impl<'a, 'b> IntoIterator for ListNodeReadUtil<'a, 'b> {
+    type Item = NodeReadUtil<'a, 'b>;
+    type IntoIter = ListNodeReadUtilIter<'a, 'b>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let mut iter = ListNodeReadUtilIter {
+            iter: self.list.iter().enumerate(),
+            pos_tree: self.pos_tree,
+        };
+        if self.index != 0 {
+            iter.iter.nth(self.index - 1);
+        }
+        iter
+    }
+}
+
+/// Iterator over the items of the list in a `ListNodeReadUtil`
+/// that produces `NodeReadUtil`.
+///
+/// This iterator is created by the `IntoIterator` implementation
+/// of `ListNodeReadUtil`.
+///
+/// # Example
+///
+/// ```
+/// use sise::sise_expr;
+///
+/// let node = sise_expr!(["a", "b"]);
+/// let node_read_util = sise_read_util::NodeReadUtil::new(&node, None);
+/// let list_read_util = node_read_util.as_list().unwrap();
+///
+/// let mut atoms = Vec::new();
+/// for item in list_read_util {
+///     atoms.push(item.as_atom().unwrap().atom());
+/// }
+/// assert_eq!(atoms, ["a", "b"]);
+/// ```
+pub struct ListNodeReadUtilIter<'a, 'b> {
+    iter: std::iter::Enumerate<std::slice::Iter<'a, sise::Node>>,
+    pos_tree: Option<&'b sise::PosTree>,
+}
+
+impl<'a, 'b> ListNodeReadUtilIter<'a, 'b> {
+    #[inline]
+    fn map_fn(i: usize, node: &'a sise::Node, pos_tree: Option<&'b sise::PosTree>)
+        -> NodeReadUtil<'a, 'b>
+    {
+        let node_pos_tree = pos_tree.map(|pos_tree| &pos_tree.children[i]);
+        NodeReadUtil::new(node, node_pos_tree)
+    }
+}
+
+impl<'a, 'b> Iterator for ListNodeReadUtilIter<'a, 'b> {
+    type Item = NodeReadUtil<'a, 'b>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|(i, node)| Self::map_fn(i, node, self.pos_tree))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+
+    fn count(self) -> usize {
+        self.iter.count()
+    }
+
+    fn last(self) -> Option<Self::Item> {
+        let iter = self.iter;
+        let pos_tree = self.pos_tree;
+        iter.last().map(|(i, node)| Self::map_fn(i, node, pos_tree))
+    }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        self.iter.nth(n).map(|(i, node)| Self::map_fn(i, node, self.pos_tree))
+    }
+}
+
+impl<'a, 'b> DoubleEndedIterator for ListNodeReadUtilIter<'a, 'b> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.iter.next_back().map(|(i, node)| Self::map_fn(i, node, self.pos_tree))
+    }
+}
+
+impl<'a, 'b> ExactSizeIterator for ListNodeReadUtilIter<'a, 'b> {
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+impl<'a, 'b> std::iter::FusedIterator for ListNodeReadUtilIter<'a, 'b> {}
--- sise-read-util/src/tests.rs
+++ sise-read-util/src/tests.rs
@@ -7,3 +7,114 @@
 
 use sise::sise_expr;
 
+#[test]
+fn test_node_as_atom() {
+    let node = sise_expr!("test");
+    let read_util = crate::NodeReadUtil::new(&node, None);
+    assert_eq!(read_util.as_atom().unwrap().atom(), "test");
+
+    let node = sise_expr!([]);
+    let read_util = crate::NodeReadUtil::new(&node, None);
+    let expected_err = crate::ReadError::ExpectedAtom { pos: None };
+    assert_eq!(read_util.as_atom().unwrap_err(), expected_err);
+}
+
+#[test]
+fn test_node_as_list() {
+    let node = sise_expr!([]);
+    let read_util = crate::NodeReadUtil::new(&node, None);
+    assert_eq!(read_util.as_list().unwrap().list(), [] as [sise::Node; 0]);
+
+    let node = sise_expr!("test");
+    let read_util = crate::NodeReadUtil::new(&node, None);
+    let expected_err = crate::ReadError::ExpectedList { pos: None };
+    assert_eq!(read_util.as_list().unwrap_err(), expected_err);
+}
+
+fn decode_as_length(atom: &str) -> Option<usize> {
+    if atom == "invalid" {
+        None
+    } else {
+        Some(atom.len())
+    }
+}
+
+#[test]
+fn test_atom_decode() {
+    let node = sise_expr!("aa");
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let atom_read_util = node_read_util.as_atom().unwrap();
+    assert_eq!(atom_read_util.decode(decode_as_length, "decode_as_length"), Ok(2));
+
+    let node = sise_expr!("invalid");
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let atom_read_util = node_read_util.as_atom().unwrap();
+    let expected_err = crate::ReadError::InvalidValue {
+        pos: None,
+        value_type: "decode_as_length".to_string(),
+    };
+    assert_eq!(atom_read_util.decode(decode_as_length, "decode_as_length"), Err(expected_err));
+}
+
+#[test]
+fn test_list_expect_end() {
+    let node = sise_expr!([]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let list_read_util = node_read_util.as_list().unwrap();
+    assert!(list_read_util.expect_end().is_ok());
+
+    let node = sise_expr!(["test"]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let list_read_util = node_read_util.as_list().unwrap();
+    let expected_err = crate::ReadError::ExpectedListEnd {
+        node_pos: None,
+    };
+    assert_eq!(list_read_util.expect_end(), Err(expected_err));
+}
+
+#[test]
+fn test_list_try_next_item() {
+    let node = sise_expr!(["test-1", "test-2"]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let mut list_read_util = node_read_util.as_list().unwrap();
+    assert_eq!(list_read_util.try_next_item().unwrap().node(), "test-1");
+    assert_eq!(list_read_util.try_next_item().unwrap().node(), "test-2");
+    assert!(list_read_util.try_next_item().is_none());
+}
+
+#[test]
+fn test_list_next_item() {
+    let node = sise_expr!(["test-1", "test-2"]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let mut list_read_util = node_read_util.as_list().unwrap();
+    assert_eq!(list_read_util.next_item().unwrap().node(), "test-1");
+    assert_eq!(list_read_util.next_item().unwrap().node(), "test-2");
+    let expected_err = crate::ReadError::ExpectedNodeInList {
+        list_pos: None,
+    };
+    assert_eq!(list_read_util.next_item().unwrap_err(), expected_err);
+}
+
+#[test]
+fn test_list_decode_atoms() {
+    let node = sise_expr!(["a", "aa"]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let list_read_util = node_read_util.as_list().unwrap();
+    let decoded = list_read_util.decode_atoms(decode_as_length, "decode_as_length", false).unwrap();
+    assert_eq!(decoded, [1, 2]);
+
+    let node = sise_expr!([]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let list_read_util = node_read_util.as_list().unwrap();
+    let decoded = list_read_util.decode_atoms(decode_as_length, "decode_as_length", true).unwrap();
+    assert_eq!(decoded, []);
+
+    let node = sise_expr!([]);
+    let node_read_util = crate::NodeReadUtil::new(&node, None);
+    let list_read_util = node_read_util.as_list().unwrap();
+    let expected_err = crate::ReadError::ExpectedNodeInList {
+        list_pos: None,
+    };
+    let result = list_read_util.decode_atoms(decode_as_length, "decode_as_length", false);
+    assert_eq!(result.unwrap_err(), expected_err);
+}
