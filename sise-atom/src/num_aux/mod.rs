0117e247a885e5b7b0ba6b8ac967bb78dae6f1a2
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2019-06-19 20:44:20 +0200

    Remove 'sise-atom', it will be available at https://github.com/eduardosm/rust-sise-atom

--- sise-atom/src/num_aux/mod.rs
+++
@@ -1,9 +0,0 @@
-// Taken from rust 3ade426ede7bca4a74bc641a12f2e7fe2cc20c47 libcore.
-
-//! Numeric traits and functions for the built-in numeric types.
-
-// All these modules are technically private and only exposed for coretests:
-pub mod flt2dec;
-pub mod dec2flt;
-pub mod bignum;
-pub mod diy_float;
