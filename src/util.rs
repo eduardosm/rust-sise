36218972a729ee7a03368dea754493b92bdc18bf
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-10-03 16:01:45 +0200

    Remove copyright headers

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,3 @@
-// Copyright 2019 Eduardo Sánchez Muñoz
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 /// Returns whether `chr` is a valid atom character outside a
 /// string (i.e. one of `:atomchar:` documented at `TreeNode::Atom`).
 pub fn is_atom_chr(chr: char) -> bool {
